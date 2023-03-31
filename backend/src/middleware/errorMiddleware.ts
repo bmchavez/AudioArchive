37ef4592f072e172fb99446b6d1d2a2b9d9e8094
Ben Chavez <ben.m.chavez@gmail.com>
2023-03-31 11:19:06 -0500

    set up concurrently to run both client and server and added 404 error handling for favicon

--- backend/src/middleware/errorMiddleware.ts
+++ backend/src/middleware/errorMiddleware.ts
@@ -2,6 +2,11 @@ import { RequestHandler } from 'express';
 
 // TODO: UPDATE AND FINISH SETTING UP ERROR HANDLING MIDDLEWARE
 const notFound: RequestHandler = (req, res, next) => {
+  if (req.url == '/favicon.ico') {
+    res.sendStatus(204); // Send an empty response with a 204 No Content status
+    return;
+  }
+
   const error = new Error(`Not Found = ${req.originalUrl}`);
   res.status(404);
   next(error);
